9beec89d05139608ccdf00d1b1ff072844fea93c
dileepnandanam <dileepsnandanam@gmail.com>
2012-12-14 11:04:46 +0530

    removedCustomID

--- shorten.py
+++ shorten.py
@@ -1,4 +1,4 @@
-import webapp2
+import webapp2,random
 
 from google.appengine.ext import db
 
@@ -7,14 +7,18 @@ class urlmapping(db.Model):
   
   smallUrl = db.StringProperty()
   bigUrl = db.StringProperty()
+  time = db.DateTimeProperty(auto_now_add=True)
 
 
 
 class MainPage(webapp2.RequestHandler):
+	
+	
 	def get(self):
 		
-		UrlIndecater=self.request.get('short')
+		UrlIndecater=self.request.path[1:]
 		if UrlIndecater:
+			self.response.write( UrlIndecater)
 			targets=urlmapping.all().filter("smallUrl =",UrlIndecater)
 			for i in targets:
 				self.redirect(str(i.bigUrl))
@@ -25,41 +29,65 @@ class MainPage(webapp2.RequestHandler):
 			self.response.write("""<html>
 				<form action="/urlstore" method="post" >
 					Big url:<input name="bigUrl" >
-					custom identifier:<input name="smallUrl" > <br>
-					<input type=submit value="shorten" >
+					
+					<input type=submit value="shorten" > 
 				</form>
 		
 		
 			</html>""")
 	
+def makeRandomID():
+	
+	res=urlmapping.all().order('-time').get()
+	if res:
+		last=str(res.smallUrl)
+	else:
+		last="A"
+		
+	res=""
+	ten=True
+	i=-1
+	while ten:
+		i+=1
+		last=last[0:i]+nextc(last[i])+last[i+1:]
+		if last[i]=='A':
+			ten=True
+			if i==len(last)-1:
+				last=last+'B'
+		else:
+			ten=False
 
+	return last
+			
+		
+def nextc(c):
+	return chr(65+(ord(c)+1-65)%58)
+	
 class UrlStore(webapp2.RequestHandler):
 	
 	def post(self):
 		self.response.headers['content-Type'] = 'html'
 		big=self.request.get('bigUrl')
-		small=self.request.get('smallUrl')
 		
-		if not small or not big:
+		
+		if not big:
 			self.redirect("/")
 			
-		same_indicators=0
 		
+		res=urlmapping.all().filter("bigUrl =",big).get()
+		if res:
+			small=res.smallUrl
+		else:
+			small=makeRandomID()
+			newurl=urlmapping(smallUrl=small,bigUrl=big)
+			newurl.put()
 		
-		for i in urlmapping.all().filter("smallUrl =",small).run(limit=9):
-			same_indicators+=1
 			
-		if same_indicators>0:
-			self.redirect("/")
 		
 		
-		newurl=urlmapping(smallUrl=small,bigUrl=big)
-		
-		newurl.put()
 		myUrl=self.request.url.split("/")
-		self.response.write(myUrl[1]+myUrl[2]+"/?"+"short="+small)
-			
+		self.response.write(myUrl[1]+myUrl[2]+"/"+small)
 		
 app = webapp2.WSGIApplication([('/', MainPage),
-								('/urlstore', UrlStore)],
+								('/urlstore', UrlStore),('/.*', MainPage)],
 															debug=True)
